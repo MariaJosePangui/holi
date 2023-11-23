74efa694f06a57074fabdbf00b81890fbe76dd20
EsTebanSC1 <estebanseron@gmail.com>
2023-11-23 01:18:01 -0300

    hola?dhfg

--- src/main/java/com/example/ej_prueba/model/hola.java
+++ src/main/java/com/example/ej_prueba/model/hola.java
@@ -12,6 +12,14 @@ public class hola {
     }
     private String nombre;
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public hola(String nombre) {
         this.nombre = nombre;
     }
