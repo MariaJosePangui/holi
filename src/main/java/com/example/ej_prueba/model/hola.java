bff6e2df49ef176f5feb571a0537598feb96f9b5
EsTebanSC1 <estebanseron@gmail.com>
2023-11-23 00:50:14 -0300

    weas

---
+++ src/main/java/com/example/ej_prueba/model/hola.java
@@ -0,0 +1,13 @@
+package com.example.ej_prueba.model;
+
+public class hola {
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
